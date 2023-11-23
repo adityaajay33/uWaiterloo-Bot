6970a965278364a55207ea588e7da2d29ffa6268
Brian Hsieh Zhang <97909955+brianhsz@users.noreply.github.com>
2023-11-23 11:54:48 -0500

    Update CYPRUS.cpp
    Cleaned up code and comments

--- CYPRUS.cpp
+++ CYPRUS.cpp
@@ -1,5 +1,5 @@
 /******************************************************************
-CYPRUS.c
+CYPRUS_CLEANED.c
 Main Program for the WaiterlooBot
 
 Written by: Aditya Ajay, Andrew Chai, Brian Zhang, and Sayan Saha
@@ -9,11 +9,11 @@ Written by: Aditya Ajay, Andrew Chai, Brian Zhang, and Sayan Saha
 
 const int RESTAURANT_HEIGHT = 2;
 const int RESTAURANT_WIDTH = 3;
-const int STANDARD_M_POWER = 50;
+const int STANDARD_M_POWER = 30;
 const int NUM_X_POS = 50;
 const int NUM_Y_POS = 50;
 const int table_locations[RESTAURANT_HEIGHT][RESTAURANT_WIDTH] = {{1,2,3},
-																																	{4,5,6}};
+								  {4,5,6}};
 const float TAX = 1.13;
 
 //******************ARRAYS******************//
@@ -26,7 +26,7 @@ typedef struct
 {
 	int row, col;
 	int rows_moved, cols_moved;
-	
+
 }Location;
 
 
@@ -39,18 +39,18 @@ typedef struct
  */
 void configureAllSensors()
 {
-  SensorType[S1] = sensorEV3_Color;
+	SensorType[S1] = sensorEV3_Color;
 	wait1Msec(50);
 	SensorMode[S1] = modeEV3Color_Color;
-  wait1Msec(50);
-  SensorType[S2] = sensorEV3_Color;
+	wait1Msec(50);
+	SensorType[S2] = sensorEV3_Color;
 	wait1Msec(50);
 	SensorMode[S2] = modeEV3Color_Color;
-  wait1Msec(50);
-  SensorType[S3] = sensorEV3_Ultrasonic;
-  wait1Msec(50);
-
-  SensorType[S4] = sensorEV3_Gyro;
+	wait1Msec(50);
+	SensorType[S3] = sensorEV3_Ultrasonic;
+	wait1Msec(50);
+	
+	SensorType[S4] = sensorEV3_Gyro;
 	wait1Msec(50);
 	SensorMode[S4] = modeEV3Gyro_Calibration;
 	wait1Msec(100);
@@ -73,64 +73,100 @@ void setMotors(int motor_power_A, int motor_power_C)
 	motor[motorC] = -motor_power_C;
 }
 
-//comment later
+/*
+ * followPath()
+ *
+ * Rocks the robot back and forth to try and find the black line.
+ * Does an initially small sweep, then if no line is found, does a 
+ * larger sweep.
+ *
+ */
+// clean up later
 void followPath()
 {
-	setMotors(0,0);
-	const int SLOW = 10;
+	const int SWEEP_SPEED = 10;
 
 	bool found_line = false;
-	const int TIME = 1000;
+	const int TIME = 700;
+
+	//small check
+	time1[T1] = 0;
+	setMotors(SWEEP_SPEED, -SWEEP_SPEED);
+	while(time1[T1] < TIME/2 && !found_line){
+		if ((SensorValue[S2] == (int)colorBlack) || (SensorValue[S2] == (int)colorRed)) {
+			found_line = true;
+		}
+	}
 
 	time1[T1] = 0;
-	motor[motorA] = SLOW;
-	motor[motorC] = -SLOW;
+	setMotors(-SWEEP_SPEED, SWEEP_SPEED);
 	while(time1[T1] < TIME && !found_line){
 		if ((SensorValue[S2] == (int)colorBlack) || (SensorValue[S2] == (int)colorRed)) {
 			found_line = true;
 		}
 	}
 
+	time1[T1] = 0;
+	setMotors(SWEEP_SPEED, -SWEEP_SPEED);
+	while(time1[T1] < TIME/2 && !found_line){
+		if ((SensorValue[S2] == (int)colorBlack) || (SensorValue[S2] == (int)colorRed)) {
+			found_line = true;
+		}
+	}
+
+	//bigger sweep
 	while (SensorValue[S2] != (int)colorBlack && !found_line) {
 		time1[T1] = 0;
-		motor[motorA] = -SLOW;
-		motor[motorC] = SLOW;
+		setMotors(SWEEP_SPEED, -SWEEP_SPEED);
+		while(time1[T1] < (TIME) && !found_line){
+			if ((SensorValue[S2] == (int)colorBlack) || (SensorValue[S2] == (int)colorRed)) {
+				found_line = true;
+			}
+		}
+
+		time1[T1] = 0;
+		setMotors(-SWEEP_SPEED, SWEEP_SPEED);
 		while(time1[T1] < 2*TIME && !found_line){
 			if ((SensorValue[S2] == (int)colorBlack) || (SensorValue[S2] == (int)colorRed)) {
 				found_line = true;
 			}
 		}
-		motor[motorA] = SLOW;
-		motor[motorC] = -SLOW;
-		time1[T1] = 0;
 
-		while(time1[T1] < 2*TIME && !found_line){
+		time1[T1] = 0;
+		setMotors(SWEEP_SPEED, -SWEEP_SPEED);
+		while(time1[T1] < (TIME) && !found_line){
 			if ((SensorValue[S2] == (int)colorBlack) || (SensorValue[S2] == (int)colorRed)) {
 				found_line = true;
 			}
 		}
 	}
-	setMotors(0,0);
 }
 
-//comment later
-bool driveForward(int motor_power)
+/*
+ * driveForward()
+ *
+ * Drives forward until it reaches a red node, following
+ * the black line along the way.
+ *
+ */
+bool driveForward()
 {
-	setMotors(motor_power, motor_power);
+	setMotors(STANDARD_M_POWER, STANDARD_M_POWER);
 	wait1Msec(300);
 
 	while(SensorValue[S2] != (int)colorRed)
 	{
 		if ((SensorValue[S2] != (int)colorBlack) && (SensorValue[S2] != (int)colorRed)) {
 			followPath();
-			setMotors(motor_power, motor_power);
-			wait1Msec(300);
+			setMotors(STANDARD_M_POWER, STANDARD_M_POWER);
+			wait1Msec(100);
 		}
 		if (SensorValue[S3] <= 10) {
 			setMotors(0,0);
 			return false;
 		}
 	}
+	wait1Msec(300);
 	setMotors(0,0);
 	return true;
 }
@@ -181,28 +217,6 @@ void getDriveLocation(int table_num, Location &target)
 }
 
 /*
- * driveToTable()
- *
- * Drives the robot forwards and turns towards to nearest table so that it
- * is ready to drop off the plate
- *
- * @param motor_power The motor power that the robot moves at.
- */
-//later change to drive to the table (a certain distance away)
-void driveToTable (int motor_power)
-{
-	setMotors(motor_power, motor_power);
-
-	driveForward(STANDARD_M_POWER);
-	wait1Msec(200);
-	setMotors(0,0);
-
-	turn(-90);
-
-	setMotors(0,0);
-}
-
-/*
  * leaveKitchen()
  *
  * Moves the robot from the kitchen area to the start of the restaurant
@@ -210,9 +224,9 @@ void driveToTable (int motor_power)
 void leaveKitchen()
 {
 	turn(180);
-	driveForward(STANDARD_M_POWER);
+	driveForward();
 	turn(90);
-	driveForward(STANDARD_M_POWER);
+	driveForward();
 
 }
 
@@ -226,30 +240,32 @@ void leaveKitchen()
  */
 bool driveToLocation (Location &target)
 {
-	leaveKitchen(); 
+	leaveKitchen();
 
 	bool completed = true;
 	for (int count = 0; count < (target.row); count++) {
-		completed = driveForward(STANDARD_M_POWER);
 		target.rows_moved++;
+		completed = driveForward();
 		if (!completed) {
 			return false;
 		}
 	}
-	
+
 	setMotors(STANDARD_M_POWER,STANDARD_M_POWER);
 	wait1Msec(300);
 	turn(90);
-	
+
 	for (int count = 0; count < (target.col)*2; count++) {
-		completed = driveForward(STANDARD_M_POWER);
+		completed = driveForward();
 		target.cols_moved++;
 		if (!completed) {
 			return false;
 		}
 	}
 
-	driveToTable(STANDARD_M_POWER);
+	driveForward();
+	wait1Msec(1000);
+	turn(-90);
 	return true;
 }
 
@@ -261,15 +277,19 @@ bool driveToLocation (Location &target)
  *
  * @param target The x and y coordinate of the table in the restaurant grid
  */
-void driveHome(Location &target)
-{
-	for (int count = 0; count < target.col - 1; count++) {
-		driveForward(STANDARD_M_POWER);
+void driveHome(Location &target) {
+	displayString(3, "%d", target.cols_moved);
+	for (int count = 0; count < target.cols_moved; count++) {
+		driveForward();
 	}
 	turn(-90);
-	for (int count = 0; count < target.row - 1; count++) {
-		driveForward(STANDARD_M_POWER);
+	for (int count = 0; count < target.rows_moved; count++) {
+		driveForward();
 	}
+
+	turn(-90);
+	driveForward();
+
 }
 
 /*
@@ -285,7 +305,12 @@ void waitButton(TEV3Buttons button)
 	while(getButtonPress(button)) {}
 }
 
-//comment later
+/*
+ * clearScreen()
+ *
+ * Clears the display screen for the Lego EV3
+ *
+ */
 void clearScreen() {
 	for (int row = 1; row < 15; row++) {
 		displayString(row, "                                        ");
@@ -319,21 +344,36 @@ int tableChoice()
 	return table_num;
 }
 
-void moveClaw(bool open) //true for open, false for close
+/*
+ * moveClaw()
+ *
+ * Opens or closes the robot claw. 
+ *
+ * @param open True for open, false for close.
+ */
+void moveClaw(bool open)
 {
-	const int ENC_LIMIT = 620;
+	const int ENC_LIMIT = 700;
 	nMotorEncoder[motorB] = 0;
 
 	if (open) {
-		motor[motorB] = 25;
+		motor[motorB] = STANDARD_M_POWER;
 	} else {
-		motor[motorB] = -25;
+		motor[motorB] = -STANDARD_M_POWER;
 	}
 	while (abs(nMotorEncoder[motorB]) < abs(ENC_LIMIT))
 	{}
 	motor[motorB] = 0;
 }
 
+/*
+ * foodOrBill()
+ *
+ * Prompts the user whether the robot is to deliver food or
+ * manually send out a bill.
+ *
+ * @return True for deliver food, false for bill
+ */
 bool foodOrBill()
 {
 	clearScreen();
@@ -358,6 +398,13 @@ bool foodOrBill()
 	return false;
 }
 
+/*
+ * getPayment()
+ *
+ * Displays the bill and accepts the customer payment.
+ *
+ * @param The table number that the bill is going to
+ */
 void getPayment(int table_num) {
 	displayBigStringAt(0, 75, "PLEASE PAY BILL");
 
@@ -368,7 +415,14 @@ void getPayment(int table_num) {
 	bill[table_num-1] = 0;
 }
 
-//comment later
+/*
+ * scanPlate()
+ *
+ * Scans the colour of the plate and adds to a table's bill
+ * depending on the colour.
+ *
+ * @param table_num The number of the table that accumulates the bill
+ */
 void scanPlate (int table_num) {
 	if (SensorValue[S1] == (int)colorBlue) {
 		bill[table_num-1] += 8;
@@ -380,7 +434,13 @@ void scanPlate (int table_num) {
 	}
 }
 
-//implement later (checks if bills are all paid)
+/*
+ * billsEmpty()
+ *
+ * Checking if all the bills are payed for.
+ *
+ * @return true if all the bills in the restaurant have been paid
+ */
 bool billsEmpty() {
 	for (int table = 0; table < RESTAURANT_HEIGHT*RESTAURANT_WIDTH; table++) {
 		if (bill[table] != 0) {
@@ -390,25 +450,20 @@ bool billsEmpty() {
 	return true;
 }
 
-void collectBills() {
-
-}
 
 
 
 
 //*****************MAIN PROGRAM******************//
 //TODO:
-//Integrate bill collection system
-//Do all laters.
+//Integrate bill collection system.
 
 task main(){
 	configureAllSensors();
-
 	bool in_use = true;
 	//main loop
 	displayBigStringAt(0, 100, "Welcome!");
-	displayString(10, "The uWaiterlooBot is at your service!");
+	displayString(10, "uWaiterlooBot, at your service!");
 
 	wait1Msec(2000);
 	clearScreen();
@@ -439,12 +494,15 @@ task main(){
 
 			//get to table
 			Location target;
+			target.rows_moved = 1;
+			target.cols_moved = 1;
 			getDriveLocation(table_num, target);
-			
+
 			bool completed_travel = true;
 			completed_travel = driveToLocation(target);
 
 			if (!completed_travel) {
+				turn(180);
 				driveHome(target);
 			}
 
@@ -455,18 +513,18 @@ task main(){
 			} else {
 				getPayment(table_num);
 			}
-
+			turn(-90);
 			driveHome(target);
 		}
 	}
 
-	
+
 	//shut down procedure
 	if (!billsEmpty()) {
 		displayString(3, "PLEEEEEAAAAASE PAY YOUR BILLS"); //insert bill collection system here
 		// new idea:
 		// how about we just go from table to table, skipping if the table is in line
-		
+
 		wait1Msec(2000);
 		//activate bill payment procedure
 	} else {
